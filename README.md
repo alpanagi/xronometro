abe392781cdfdf2fcf2ea51d30aa3b21626e730b
Alexandros Panagiotakis <dev@alpanagi.com>
2024-09-13 14:24:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Countdown Timer for the Terminal
+
+# Usage
+
+```
+timer HH:MM:SS
+```
+
+**Examples**:
+
+```
+timer 123
+timer 11:11
+timer 12:34:56
+```
+
+> Note that the values for hours/minutes/seconds are not limited to `00`-`59`. `300:865` is also valid.
---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <regex>
+#include <thread>
+
+void die() {
+  std::cerr << "Invalid argument\n" << "Usage: timer HH:MM:SS" << std::endl;
+  exit(1);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    die();
+  }
+
+  std::string input(argv[1]);
+  std::regex input_regex{"(?:(\\d+):)?(?:(\\d+):)?(\\d+)"};
+  std::smatch matches;
+  if (!std::regex_match(input, matches, input_regex)) {
+    die();
+  }
+
+  int hours = 0, minutes = 0, seconds = 0;
+  std::cout << "SIZE " << matches[2].str().length() << std::endl;
+  if (matches[2].str().length() == 0) {
+    minutes = std::atoi(matches[1].str().c_str());
+  } else {
+    hours = std::atoi(matches[1].str().c_str());
+    minutes = std::atoi(matches[2].str().c_str());
+  }
+  seconds = std::atoi(matches[3].str().c_str());
+
+  int duration = hours * 3600 + minutes * 60 + seconds;
+  while (duration > 0) {
+    hours = duration / 3600;
+    minutes = (duration % 3600) / 60;
+    seconds = (duration % 60);
+
+    std::cout << "\r";
+    std::cout << std::setfill('0') << std::setw(2) << hours << ':';
+    std::cout << std::setfill('0') << std::setw(2) << minutes << ':';
+    std::cout << std::setfill('0') << std::setw(2) << seconds << std::flush;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    duration--;
+  }
+  std::cout << std::endl;
+}
